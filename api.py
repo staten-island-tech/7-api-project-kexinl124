28910963295ad5e6b5ecbce4e0a256d7cf0209c7
kexinl124 <KExinl10@nycstudents.net>
2025-12-17 09:26:17 -0500

    Update api.py

--- api.py
+++ api.py
@@ -3,17 +3,20 @@ import requests
 def getCountries(country):
     response = requests.get(f"https://countriesnow.space/api/v0.1/countries/population/cities{country.lower()}")
     if response.status_code != 200:
-        print("Error fetching data!")
+        print("Sorry, error fetching data!")
         return None
     
     data = response.json()
     return {
-        "country": data["country"],
         "city": data["city"],
-        "population": data["populationCounts"],
-        "reliablity":data["reliablity"],
-        
+        "country": data["country"],
+        "populationCounts": data["populationCounts"],
+        "types": [c["type"]["city"] for c in data["types"]]
     }
 
 countries = getCountries("Åland Islands")
 print(countries)
+
+
+def COuntries():
+    countrIes = input("Which countries do you want to know the population from?")
